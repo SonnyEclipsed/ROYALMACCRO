b3da89e5adba9c38389644901005d6eae7b87d96
Connor Haley <c.haley@ufl.edu>
2025-02-13 22:32:49 -0500

    goback

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from uuid import uuid4
 
 app = Flask(__name__)
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Ensure they are set
-if SECRET_KEY is None or SECRET_KEY.strip() == "":
-    raise ValueError("❌ SECRET_KEY is missing! Make sure it's set in Railway's environment variables.")
+# if SECRET_KEY is None or SECRET_KEY.strip() == "":
+#     raise ValueError("❌ SECRET_KEY is missing! Make sure it's set in Railway's environment variables.")
 
 if not DATABASE_URL:
     raise ValueError("❌ Missing environment variable! Set DATABASE_URL.")
